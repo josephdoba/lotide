bb4ccacf0390d048b880645f40c3d452a9e475ba
Joseph Doba <josephdoba@gmail.com>
2022-03-23 20:01:18 -0700

    Created Assertion test with emoji's

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("illegal", "Illegal");
+assertEqual("Apples","Oranges");
+assertEqual(10,10);
+assertEqual(30,70);
