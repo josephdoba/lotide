791215771498389bf0cb6846e98078bee133dd6b
Joseph Doba <josephdoba@gmail.com>
2022-03-25 15:38:25 -0700

    Created eqArrays.js for lotide

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let result = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i <= arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        result = true;
+      } else {
+        return result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
