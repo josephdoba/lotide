1022bc8cae6848c0578f6afd52e85b2e31709b3a
Joseph Doba <josephdoba@gmail.com>
2022-03-25 15:54:37 -0700

    Fixed bug occuring when a true value was found after a false one. Added break statements

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let result = false;
   if (arr1.length === arr2.length) {
@@ -27,5 +19,3 @@ const eqArrays = function(arr1, arr2) {
 };
 
 eqArrays([1,2,3],[1,2,3]);
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
