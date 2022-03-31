4105bebf7c3288049fc836260a92b23a2ea05d37
Joseph Doba <josephdoba@gmail.com>
2022-03-30 19:55:46 -0700

    countLetter.js created

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(words) {
+  let result = {};
+  for (let letter of words) {
+    if (letter !== " ") {
+      // if (result[letter]) {  // if it exists
+      //   result[letter] += 1;
+      // } else {
+      //   result[letter] = 1;
+      // }
+      result[letter] ? result[letter] += 1 : result[letter] = 1;
+
+    }
+  }
+  console.log(result);
+};
+countLetters("lighthouse in the house");
+
