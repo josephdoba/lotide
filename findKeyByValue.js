c4c25701837c2edb46eab6d942c59ff8244578d8
Joseph Doba <josephdoba@gmail.com>
2022-03-31 19:50:41 -0700

    findKeyByValue.js created

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, valueSearch) {
+  for (let search of Object.keys(object)) {
+    if (object[search] === valueSearch) {
+      console.log(search);
+      return search;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
